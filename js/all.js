84c06964c65da4dea26700289d8ba252d08b8e4e
fsyp2018 <45746305+fsyp2018@users.noreply.github.com>
2019-05-09 16:26:50 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
-	$('.menu-mobile').on('click', function(event) {
-		event.preventDefault();
-		$('body').toggleClass('menu-show');
+
+	$(".menu-icon").click(function() { 
+	  $(this).toggleClass("animated");
+	  $(".inside-menu").slideToggle(500);
 	});
-});
+
+	$('.food-ani').click(function(){
+        $('html,body').animate({scrollTop:$('.content').offset().top},800);
+	});
+
+	$('.chef-ani').click(function(){
+        $('html,body').animate({scrollTop:$('.chef').offset().top},800);
+	});
+
+	$('.openmap-ani').click(function(){
+        $('html,body').animate({scrollTop:$('.openmap').offset().top},800);
+	});
+	
+	$("#gotop").click(function(){
+		jQuery("html,body").animate({
+			scrollTop:0
+		},1000);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 500){
+			$('#gotop').fadeIn("fast");
+		} else {
+			$('#gotop').stop().fadeOut("fast");
+		}
+	});
+
+  });
